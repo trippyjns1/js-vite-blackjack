9e75146210cb4228391b34b436616912ef0cb6f6
trippyjns1 <jhaderinfante4@gmail.com>
2026-06-19 16:10:02 -0500

    first commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-carta';
+export {crearCartaHTML} from './crear-cartaHTML'
+export {turnoComputadora} from './turno-computadora';
